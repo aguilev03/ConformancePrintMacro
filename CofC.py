2b7a55b778bb5adcc364d013ad5c381fed84bb74
Evan Aguilar <evan@eaguil.com>
2018-03-29 06:08:55 -0500

    website
    initial push

---
+++ CofC.py
@@ -0,0 +1,89 @@
+# Script that will make a copy of a template to another directory for archive and Edit.
+# Leaving template unchanged.
+
+# import necessary libraries 
+import os
+import datetime
+import openpyxl
+import shutil
+import pyautogui
+import time
+
+# This finds the absolute path of the directory
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+#link to template excel file
+exfile = os.path.join(THIS_FOLDER, 'C of C NEW 2018 (version 1).xlsm')
+
+# This makes a copy of the excel file named year, month, day and minute 
+def createNew() :
+    global newFileName
+    global today
+   
+    today = datetime.datetime.now()
+    newFileName ='C of C New ' + today.strftime("%Y_%m%d%M") + ".xlsm"
+    os.chdir(THIS_FOLDER + '\\Saved\\')
+    shutil.copyfile(exfile, newFileName)
+
+# starts excel and open new editable file 
+def startProg(var) :
+    os.startfile(var)
+    
+def loadxcl() :
+   
+    wb = openpyxl.load_workbook(newFileName, read_only=False, keep_vba=True)
+    sheet = wb['Scan']
+    sheet2 = wb['CofC']
+
+    def splitStr(a, b, c) :
+        prefixStr = sheet[a].value.split()
+        sheet2[b] = prefixStr[c]
+        
+
+    
+    splitStr('A1','D6',0)
+    sheet2['J6']= today.strftime("%m/%d/%Y")
+    
+    for x in range(50) :
+        y = 'A' + str(x+1)
+           
+        if x < 20 :
+            d = 'B' + str(x + 10)
+            splitStr(y,d,1)
+        
+            
+
+        elif x >= 20 and x < 40 :
+            d = 'F' + str(x - 10)
+            splitStr(y,d,1)
+        
+            
+
+        else: 
+            d = 'J' + str(x - 30)
+            splitStr(y,d,1)
+            
+    wb.save(newFileName)
+
+def printxcl() :
+    def mpress(x,y) :
+        pyautogui.keyDown(x)
+        pyautogui.press(y)
+        pyautogui.keyUp(x)
+        time.sleep(2)
+        return
+
+    time.sleep(5)
+    mpress('ctrl','t')
+    time.sleep(30)
+    mpress('ctrl', 'p')
+
+createNew()
+startProg(newFileName)
+pausesys = input("Press Enter after you finish scanning to continue")
+loadxcl()
+startProg(newFileName)
+printxcl()
+
+
+    
